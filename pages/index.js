46e116a565936f3418ded9964c747c42e5e37d84
yenyin262 <yenyinf287@gmail.com>
2021-10-14 21:54:02 -0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
 
-export default function Home() {
+import { printful } from "../lib/printful-client";
+import styles from "../styles/Home.module.css";
+
+export default function Home({ products }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,43 +14,12 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          {" "}
+          {products.map((product, id) => {
+            console.log(product, "product here");
+            return <div>{product.name}</div>;
+          })}{" "}
         </div>
       </main>
 
@@ -58,12 +29,38 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const { result: productIds } = await printful.get("sync/products");
+
+  const allProducts = await Promise.all(
+    productIds.map(async ({ id }) => {
+      return await printful.get(`sync/products/${id}`);
+    })
+  );
+
+  const products = allProducts.map(
+    ({ result: { sync_product, sync_variants } }) => ({
+      ...sync_product,
+      variants: sync_variants.map(({ name, ...variant }) => ({
+        name,
+        ...variant,
+      })),
+    })
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
 }
