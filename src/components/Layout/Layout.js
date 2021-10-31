d99afe3bf9b8a46587bb34d0d90b235177a69bc7
yenyin262 <yenyinf287@gmail.com>
2021-10-30 22:52:29 -0700

    remove divs

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,10 +4,8 @@ import NavBar from "../NavBar/NavBar";
 export default function Layout({ children }) {
   return (
     <>
-      <div>
-        <NavBar />
-        {children}
-      </div>
+      <NavBar />
+      {children}
     </>
   );
 }
