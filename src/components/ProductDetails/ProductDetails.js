ec44a36e3d50f8ff87212f12ab59c2c39f663184
yenyin262 <yenyinf287@gmail.com>
2021-10-30 22:51:00 -0700

    Add Product Info component onto Product Details Page

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import styles from "./ProductDetails.module.css";
 import classNames from "classnames";
 import LikeList from "../LikeList/LikeList";
+import ProductInfo from "../ProductInfo/ProductInfo";
 
 export default function ProductDetails({ product, otherProducts }) {
   console.log(product, "product details");
@@ -12,61 +13,66 @@ export default function ProductDetails({ product, otherProducts }) {
     ({ type }) => type === "preview"
   );
 
-  const handleProductSelect = (e) => {
-    setSelectedProductIdx(e.target.value);
-  };
-
   return (
-    <div className={styles.gridContainer}>
-      <div className={styles.imgItem}>
-        <Image
-          layout="responsive"
-          width={450}
-          height={450}
-          src={selectedProductFile.preview_url}
-          className={styles.productImg}
+    <div>
+      <div className={styles.gridContainer}>
+        <div className={styles.imgItem}>
+          <Image
+            layout="responsive"
+            width={450}
+            height={450}
+            src={selectedProductFile.preview_url}
+            className={styles.productImg}
+          />
+        </div>
+
+        <ProductInfo
+          product={product}
+          selectedProductIdx={selectedProductIdx}
+          selectedProductFile={selectedProductFile}
+          setSelectedProductIdx={setSelectedProductIdx}
         />
-      </div>
-
-      <div className={styles.gridInfoItem}>
-        <div className={styles.productName}>{product.name}</div>
-        <div className={styles.productPrice}>
-          {product.variants[selectedProductIdx].retail_price}
-        </div>
-
-        <div className={styles.productSizeInfoContainer}>
-          <label className={styles.sizeLabel}>Style/Size</label>
-          <select
-            value={selectedProductIdx}
-            className={styles.selectField}
-            onChange={handleProductSelect}
-          >
-            {product.variants.map((variant, index) => (
-              <option key={variant.id} value={index}>
-                {getProductName(variant.name)} - {variant.retail_price}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <button
-          className={classNames(`snipcart-add-item`, styles.cartBtn)}
-          data-item-id={product.variants[selectedProductIdx].external_id}
-          data-item-price={product.variants[selectedProductIdx].retail_price}
-          data-item-url="/[productId]"
-          data-item-description={`SKU: ${product.variants[selectedProductIdx].sku}`}
-          data-item-image={selectedProductFile.preview_url}
-          data-item-name={product.variants[selectedProductIdx].name}
-          data-item-custom1-name="Gift note"
-        >
-          <div>
-            <span className={styles.btnText}>Add to cart</span>
+        {/* 
+        <div className={styles.gridInfoItem}>
+          <div className={styles.productName}>{product.name}</div>
+          <div className={styles.productPrice}>
+            {product.variants[selectedProductIdx].retail_price}
           </div>
-        </button>
-      </div>
-      <div>
-        <div>You might Also Like </div>
 
+          <div className={styles.productSizeInfoContainer}>
+            <label className={styles.sizeLabel}>Style/Size</label>
+            <select
+              value={selectedProductIdx}
+              className={styles.selectField}
+              onChange={handleProductSelect}
+            >
+              {product.variants.map((variant, index) => (
+                <option key={variant.id} value={index}>
+                  {getProductName(variant.name)} - {variant.retail_price}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <button
+            className={classNames(`snipcart-add-item`, styles.cartBtn)}
+            data-item-id={product.variants[selectedProductIdx].external_id}
+            data-item-price={product.variants[selectedProductIdx].retail_price}
+            data-item-url="/[productId]"
+            data-item-description={`SKU: ${product.variants[selectedProductIdx].sku}`}
+            data-item-image={selectedProductFile.preview_url}
+            data-item-name={product.variants[selectedProductIdx].name}
+            data-item-custom1-name="Gift note"
+          >
+            <div>
+              <span className={styles.btnText}>Add to cart</span>
+            </div>
+          </button>
+        </div> */}
+      </div>
+
+      <div className={styles.title}>You might also like </div>
+      <div className={styles[`likeList-container`]}>
         {otherProducts.map((product) => {
           return <LikeList key={product.id} product={product} />;
         })}
