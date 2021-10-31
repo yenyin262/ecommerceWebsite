2702756565fa9d88d9421d7a40c0958b11df7523
yenyin262 <yenyinf287@gmail.com>
2021-10-30 22:49:38 -0700

    Add LikeList functionality

--- src/components/LikeList/LikeList.js
+++ src/components/LikeList/LikeList.js
@@ -1,38 +1,39 @@
 import next from "next";
-import { imageConfigDefault } from "next/dist/server/image-config";
 import React, { useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
-import { getProductName } from "../../lib/getProductName";
 import styles from "./LikeList.module.css";
-
-// create active state
-// print price that is in active state and active state is an index
-// active state on Image
+import ExpressCart from "../ExpressCart/ExpressCart";
 
 export default function LikeList({ product }) {
   console.log(product, "product here ");
-  const selectedProductFile = product.variants[0].files.find(
+  const [selectedProductIdx, setSelectedProductIdx] = useState(0);
+  const selectedProductFile = product.variants[selectedProductIdx].files.find(
     ({ type }) => type === "preview"
   );
+
   return (
-    <div>
-      <Link href={`/${product.id}`}>
-        <a className={styles.gridItemContainer}>
-          <Image
-            width={150}
-            height={150}
-            src={selectedProductFile.preview_url}
-            className={styles.productImg}
-          />
-          <div className={styles.productdetailsContainer}>
-            <div className={styles[`product-name`]}>{product.name}</div>
-            <div className={styles[`product-price`]}>
-              {product.variants[0].retail_price}
-            </div>
+    <div className={styles.itemContainer}>
+      <a className={styles.gridItemContainer}>
+        <Image
+          width={200}
+          height={200}
+          src={selectedProductFile.preview_url}
+          className={styles.productImg}
+        />
+        <div className={styles.productdetailsContainer}>
+          <div className={styles[`product-name`]}>{product.name}</div>
+          <div className={styles[`product-price`]}>
+            {product.variants[0].retail_price}
           </div>
-        </a>
-      </Link>
+        </div>
+        <div className={styles.expressCartContainer}>
+          <ExpressCart
+            selectedProductIdx={selectedProductIdx}
+            setSelectedProductIdx={setSelectedProductIdx}
+            product={product}
+          />
+        </div>
+      </a>
     </div>
   );
 }
