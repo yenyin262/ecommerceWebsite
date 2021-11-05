bb58181e89c5ca928b5ac17d1268f618ae64c40c
yenyin262 <yenyinf287@gmail.com>
2021-11-04 22:52:41 -0700

    Add Flex style

--- src/components/ProductInfo/ProductInfo.js
+++ src/components/ProductInfo/ProductInfo.js
@@ -33,21 +33,22 @@ export default function ProductInfo({
           ))}
         </select>
       </div>
-
-      <button
-        className={classNames(`snipcart-add-item`, styles.cartBtn)}
-        data-item-id={product.variants[selectedProductIdx].external_id}
-        data-item-price={product.variants[selectedProductIdx].retail_price}
-        data-item-url="/[productId]"
-        data-item-description={`SKU: ${product.variants[selectedProductIdx].sku}`}
-        data-item-image={selectedProductFile.preview_url}
-        data-item-name={product.variants[selectedProductIdx].name}
-        data-item-custom1-name="Gift note"
-      >
-        <div>
-          <span className={styles.btnText}>Add to cart</span>
-        </div>
-      </button>
+      <div style={{ display: "flex" }}>
+        <button
+          className={classNames(`snipcart-add-item`, styles.cartBtn)}
+          data-item-id={product.variants[selectedProductIdx].external_id}
+          data-item-price={product.variants[selectedProductIdx].retail_price}
+          data-item-url="/[productId]"
+          data-item-description={`SKU: ${product.variants[selectedProductIdx].sku}`}
+          data-item-image={selectedProductFile.preview_url}
+          data-item-name={product.variants[selectedProductIdx].name}
+          data-item-custom1-name="Gift note"
+        >
+          <div>
+            <span className={styles.btnText}>Add to cart</span>
+          </div>
+        </button>
+      </div>
     </div>
   );
 }
