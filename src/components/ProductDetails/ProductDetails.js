04a5c6dc5e0a6846015110046327076cd6a320fd
yenyin262 <yenyinf287@gmail.com>
2021-11-15 13:44:29 -0800

    Add wish list functionality and styles

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -3,14 +3,23 @@ import Image from "next/image";
 import styles from "./ProductDetails.module.css";
 import ProductInfo from "../ProductInfo/ProductInfo";
 import Carousel from "../Carousel/Carousel";
+import useWishlistState from "../../hooks/useWishlistState";
+import useWishlistDispatch from "../../hooks/useWishlistDispatch";
+import HeartButton from "../HeartButton/HeartButton";
 
 export default function ProductDetails({ product, otherProducts }) {
-  console.log(product, "product details");
   const [selectedProductIdx, setSelectedProductIdx] = useState(0);
   const selectedProductFile = product.variants[selectedProductIdx].files.find(
     ({ type }) => type === "preview"
   );
 
+  const { addItem, removeItem } = useWishlistDispatch();
+  const { isSaved } = useWishlistState();
+  const inWishlist = isSaved(product.id);
+
+  const toggleWishlist = () => {
+    inWishlist ? removeItem(product.id) : addItem(product);
+  };
   return (
     <div>
       <div className={styles.gridContainer}>
@@ -22,6 +31,7 @@ export default function ProductDetails({ product, otherProducts }) {
             src={selectedProductFile.preview_url}
             className={styles.productImg}
           />
+          <HeartButton onClick={toggleWishlist} filled={inWishlist} />
         </div>
 
         <ProductInfo
