cb93ee1de0d43c451444bf377020e55c166f4a1f
yenyin262 <yenyinf287@gmail.com>
2021-11-15 13:50:45 -0800

    add wishlist functionality

--- src/components/LikeList/LikeList.js
+++ src/components/LikeList/LikeList.js
@@ -3,23 +3,37 @@ import React, { useState } from "react";
 import Image from "next/image";
 import styles from "./LikeList.module.css";
 import ExpressCart from "../ExpressCart/ExpressCart";
+import useWishlistDispatch from "../../hooks/useWishlistDispatch";
+import useWishlistState from "../../hooks/useWishlistState";
+import HeartButton from "../HeartButton/HeartButton";
 
 export default function LikeList({ product }) {
-  console.log(product, "product here ");
   const [selectedProductIdx, setSelectedProductIdx] = useState(0);
   const selectedProductFile = product.variants[selectedProductIdx].files.find(
     ({ type }) => type === "preview"
   );
 
+  const { addItem, removeItem } = useWishlistDispatch();
+  const { isSaved } = useWishlistState();
+  const inWishlist = isSaved(product.id);
+
+  const toggleWishlist = () => {
+    inWishlist ? removeItem(product.id) : addItem(product);
+  };
+
   return (
     <div className={styles.itemContainer}>
       <a className={styles.gridItemContainer}>
-        <Image
-          width={200}
-          height={200}
-          src={selectedProductFile.preview_url}
-          className={styles.productImg}
-        />
+        <div className={styles.imgItem}>
+          <Image
+            width={200}
+            height={200}
+            src={selectedProductFile.preview_url}
+            className={styles.productImg}
+          />{" "}
+          <HeartButton onClick={toggleWishlist} filled={inWishlist} />
+        </div>
+
         <div className={styles.productdetailsContainer}>
           <div className={styles[`product-name`]}>{product.name}</div>
           <div className={styles[`product-price`]}>
