ce8e32fd0a7ac160764be625452903168324fe6d
yenyin262 <yenyinf287@gmail.com>
2021-11-15 13:38:54 -0800

    Add Social Media Icons

---
+++ src/components/SocialMediaIcons/SocialMediaIcons.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Twitter from "../Twitter";
+import Facebook from "../Facebook";
+import Instagram from "../Instagram";
+
+export default function SocialMediaIcons() {
+  return (
+    <div>
+      <Twitter />
+      <Facebook />
+      <Instagram />
+    </div>
+  );
+}
