5d65d38793cd1ba0aac3afb2122c8f4e99906507
yenyin262 <yenyinf287@gmail.com>
2021-12-04 10:38:48 -0800

    correct the file name import

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SocialMediaIcons from "../SocialMediaIcons/SocialMediaIcons";
-import styles from "./footer.module.css";
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
