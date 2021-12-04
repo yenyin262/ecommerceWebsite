e12bc57d73ba41e334f883f7cd546a91f05d684c
yenyin262 <yenyinf287@gmail.com>
2021-12-04 11:42:02 -0800

    Update Motto line

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,8 +6,8 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div style={{ maxWidth: "500px", marginBottom: "20px" }}>
-        <strong> Psalm Series</strong> Bring your ideas to life with rewards,
-        inspiration, discounts and a few surprises along the way. Learn more.
+        <strong> Psalm Series</strong> Bringing inspiration words to you, and
+        laughter along the way. Learn more.
         <br />
         <span> Shop now </span>
       </div>
