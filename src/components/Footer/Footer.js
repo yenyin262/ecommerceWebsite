4123925a892c887b9cef0e3d1b6aaa19c5596900
yenyin262 <yenyinf287@gmail.com>
2021-12-05 21:03:02 -0800

    update styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,12 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div style={{ maxWidth: "500px", marginBottom: "20px" }}>
-        <strong> Psalm Series</strong> Bringing inspiration words to you, and
-        laughter along the way. Learn more.
-        <br />
+      <div className={styles.container}>
+        <div className={styles.motto}>
+          <strong> Psalm Series</strong> Bringing inspiration words to you, and
+          laughter along the way. Learn more.
+          <br />
+        </div>
         <span> Shop with us</span>
       </div>
       <SocialMediaIcons />
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,15 @@
-import React from "react";
 import NavBar from "../NavBar/NavBar";
-
 import PropTypes from "prop-types";
 import Footer from "../Footer/Footer";
 import Banner from "../Banner/Banner";
+import styles from "../../../styles/layout.module.css";
 
 export default function Layout({ children }) {
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+    <div className={styles.container}>
       <Banner />
       <NavBar />
-      <main style={{ flex: 1 }}>{children}</main>
+      <main className={styles.main}>{children}</main>
       <Footer />
       <Banner />
     </div>
